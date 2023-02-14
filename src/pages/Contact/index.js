fb84a1dc2cb1d1df226f78e5c043d5b4d9ec9a68
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2023-02-14 20:30:17 -0300

    implementação de rotas

---
+++ src/pages/Contact/index.js
@@ -0,0 +1,12 @@
+import Navbar from '../../components/Navbar'
+
+const Contact = () => {
+  return(
+    <div>
+      <Navbar />
+      <h1>Contato</h1>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+import Home from './pages/Home';
+import Contact from './pages/Contact'
+
+
+const Router = () => {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contato" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
