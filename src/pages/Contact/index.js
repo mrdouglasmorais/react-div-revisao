6d4ae13fe3df99f395cad225ceeae5e387fb8150
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2023-02-15 20:17:54 -0300

    finalizando formulario

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,10 +1,39 @@
 import Navbar from '../../components/Navbar'
 
+import { useState } from 'react'
+
 const Contact = () => {
+  const [ data, setData ] = useState({})
+  const submitFormData = (e) => {
+    e.preventDefault()
+    console.log(data)
+  }
+
   return(
     <div>
       <Navbar />
       <h1>Contato</h1>
+      <form onSubmit={submitFormData}>
+        <input 
+          type='text' 
+          placeholder='Nome'
+          onChange={ e => setData({...data, nome: e.target.value})}
+        />
+        <input 
+          type='text' 
+          placeholder='Email'
+          onChange={ e => setData({...data, email: e.target.value})}
+        />
+        <input 
+          type='text' 
+          placeholder='Telefone'
+          onChange={ e => setData({...data, telefone: e.target.value})}
+        />
+        <input 
+          type='submit' 
+          value='Cadastrar'
+        />
+      </form>
     </div>
   )
 }
