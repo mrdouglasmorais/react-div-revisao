7e0cc66d1cfbd8f8bddfd7f10b13ece5396289be
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2023-02-28 20:13:47 -0300

    conectando em API rest

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,39 @@
 import Navbar from "../../components/Navbar"
 import Loader from '../../components/Loader'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const Home = () => {
   const [ isLoad, setIsLoad ] = useState(false);
+  const [ data, setData ] = useState([]);
 
-  const hadleStateComponent = () => {
-    setIsLoad(!isLoad)
-  }
+  useEffect( () => {
+    setIsLoad(true)
+    axios.get('https://rickandmortyapi.com/api/character')
+    .then(
+      res => {
+        setData(res.data.results)
+      }
+    )
+    .catch( e => console.log("Erro", e))
+    .finally(() => setIsLoad(false))
+  }, [])
 
   return(
     <div>
       <Navbar />
       <h1>Home</h1>
       <p>Componente: </p>
+      {data.map( (el, index) => (
+        <div key={index}>
+          <h6>{el.name}</h6>
+          <img src={el.image} alt={el.name}/>
+          <p>{el.gender}</p>
+          <p>{el.origin?.name}</p>
+          <p>Episódios {el.episode.length}</p>
+        </div>
+      ))}
       <Loader load={isLoad} />
-      <button onClick={hadleStateComponent}>Mudar estado</button>
     </div>
   )
 }
